adf65c8d612a564bfdd6553e8576b97b4aa6f0f9
Edgar Saltillo <edsaltillo@gmail.com>
2026-02-11 10:48:48 -0600

    Feat:Integracion del proyecto en spring

---
+++ Bloom-Studio/src/test/java/com/generation/Bloom_Studio/BloomStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.Bloom_Studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
