f00a6d9c5a04d068510030afb56a79221c05ab38
Edgar Saltillo <edsaltillo@gmail.com>
2026-02-12 17:10:31 -0600

    Feat: Se generan los metodos de inventario para crear, actualizar, incrementar, decrementar y obtener

--- Bloom-Studio/src/main/java/com/generation/Bloom_Studio/service/InventoryService.java
+++ Bloom-Studio/src/main/java/com/generation/Bloom_Studio/service/InventoryService.java
@@ -1,4 +1,14 @@
 package com.generation.Bloom_Studio.service;
 
+import com.generation.Bloom_Studio.model.Inventory;
+
+import java.util.List;
+
 public interface InventoryService {
+
+    Inventory crearOActualizarCantidad(Long productoId, Long colorId, Long tallaId, Integer cantidad);
+    Inventory incrementar(Long inventarioId, Integer delta);
+    Inventory decrementar(Long inventarioId, Integer delta);
+    Inventory obtenerPorId(Long inventarioId);
+    List<Inventory> listaPorProducto(Long productoId);
 }
