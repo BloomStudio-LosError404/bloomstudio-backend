a421c6a6322ef0a2852f3213073e034261c1e9bd
HectorMagdaleno <estoreals@gmail.com>
2026-02-18 02:04:20 -0600

    actualizacion de restcontroler *

--- Bloom-Studio/src/main/java/com/generation/Bloom_Studio/controller/ColorsController.java
+++ Bloom-Studio/src/main/java/com/generation/Bloom_Studio/controller/ColorsController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/colores")
-
+@CrossOrigin(origins = "*")
 public class ColorsController {
 
     private final ColorService colorService;
