1d3f8c048919e2629236b3e08ae39da8eab60f48
HectorMagdaleno <estoreals@gmail.com>
2026-02-19 03:15:14 -0600

    matenme

--- Bloom-Studio/src/main/java/com/generation/Bloom_Studio/controller/ColorsController.java
+++ Bloom-Studio/src/main/java/com/generation/Bloom_Studio/controller/ColorsController.java
@@ -41,11 +41,10 @@ public class ColorsController {
     }
 
     // PUT actualizar color
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Colors> updateColor(@RequestBody Colors color, @PathVariable Long id){
         Colors colorActualizado = colorService.updateColor(color, id);
         return ResponseEntity.ok(colorActualizado);
-
     }
 
     // DELETE (soft delete)
